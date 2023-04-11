ede890127d87b7250e5852968625808fd0262dff
Dama Dhananjaya Daliman <19622002@mahasiswa.itb.ac.id>
2023-04-11 15:47:22 +0700

    added API routes

--- B_server/server.js
+++ B_server/server.js
@@ -1,10 +1,26 @@
-const express = require('express');
-const app = express();
+// required packages
+require('dotenv').config()
+const express = require('express')
+const mealRoutes = require('./routes/meals')
 
+// Initializing express app
+const app = express()
+
+// middleware
+app.use(express.json()) //access request body
+
+app.use((req, res, next) =>{
+  console.log(req.path, req.method)
+  next()
+})
+
+// routing
 app.get('/', (req, res) => {
-  res.send('Hello, world!');
+  res.json({mssg: 'Hello, world!'})
 });
+app.use('/api/meals', mealRoutes)
 
-app.listen(3000, () => {
-  console.log('Server is listening on port 3000');
+// listening to requests
+app.listen(process.env.PORT, () => {
+  console.log('Server is listening on port 3000!')
 });
